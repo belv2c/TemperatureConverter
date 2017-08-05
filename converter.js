58d50bfcd14c7f9b86445bbd0fbe681dbfe34bef
Brooke Lewis <belv2c@gmail.com>
2017-08-05 12:27:54 -0500

    converter works

--- converter.js
+++ converter.js
@@ -1,42 +1,35 @@
-var toFahrenheit
-var toCelsius
-var tempInput
-var 
+var tempInput = document.getElementById("tempInput");
+var radioCelsius = document.getElementById("radioCelsius");
+var radioFahrenheit = document.getElementById("radioFahrenheit");
+var returnTemp = document.getElementById("returnTemp");
+var domstring ='';
 
-
-
-
-function toCelsius (tempInput) {
-	var celcius = (tempInput - 32) / 1.8;
-	return celcius;
+function toCelsius (tempInput2) {
+    var tempInput = parseInt(tempInput2.value);
+    var celsius = (tempInput - 32) / 1.8;
+    returnTemp.innerHTML = celsius;
 }
 
-function toFahrenheit (tempInput) {
-	var farenheit = (tempInput * 1.8) + 32;
-	return farenheit;
+function toFahrenheit (tempInput2) {
+    var tempInput = parseInt(tempInput2.value);
+    var fahrenheit = (tempInput * 1.8) + 32;
+    returnTemp.innerHTML = fahrenheit;
 }
 
-function convertTemp() {
-	if(radioCelcius.checked) {
-		returnedTemp = toCelsius(tempInput) 
-	} else if (radioFarenheit.checked) {
-		returnedTemp = toFahrenheit(tempInput);
-	} else if (radioCelcius == false &&
-		radioFarenheit == false) {
-		alert("Select Celcius or Farenheit");
-	}
+function determineConverter () {
+    if (radioCelsius.checked) {
+        returnTemp = toCelsius(tempInput);
+    } else if (radioFahrenheit.checked) {
+        returnTemp = toFahrenheit(tempInput);
+        domstring += '<h2 id="returnTemp">' + returnTemp + '</h2>';
+    } else if (radioCelsius === false && radioFahrenheit === false){
+        alert ("Select Celsius or Fahrenheit to convert your temperture.");
+    }
+    console.log("this is the convert temp function");
+}
+function resetForm() {
+    document.getElementById("tempForm").reset();
+    
 }
 
-
-
-// Get a reference to the button element in the DOM
-var button = document.getElementById("convertButton");
-
-// This function should determine which conversion should
-// happen based on which radio button is selected.
-function determineConverter (convertTemp) {
-  console.log("event", clickEvent);
-}
-
-// Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter);
+document.getElementById("determineConverter").addEventListener("click", determineConverter);
